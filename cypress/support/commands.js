c8492abc765aa40041ddfa74a0f62ef163570a11
Victor Leao <leaovic@gmail.com>
2024-04-24 15:25:28 -0300

    ci github

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
 Cypress.Commands.add('login', () => { 
     const email = Cypress.env('user_name')
     const password = Cypress.env('user_password')
@@ -23,15 +11,3 @@ Cypress.Commands.add('login', () => {
     cy.get('.sign-out-link').click();
 
  })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
