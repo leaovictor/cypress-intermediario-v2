9156e4dc8b936ad4f679a9df20fbea0a61597839
Victor Leao <leaovic@gmail.com>
2024-04-28 18:37:32 -0300

    wip ci

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,16 @@
 Cypress.Commands.add('login', () => {
-    const email = Cypress.env('user_name') || 'user_name'; // Se 'USER_NAME' não estiver definido, use um valor padrão
-    const password = Cypress.env('user_password') || 'user_password'; // Se 'USER_PASSWORD' não estiver definido, use um valor padrão
+    // Recuperar o email do GitHub
+    const email = Cypress.env('user_name') || 'USER_NAME'; // Se 'user_name' não estiver definido, use um valor padrão
+
+    // Recuperar a senha do GitHub
+    const password = Cypress.env('user_password') || 'USER_PASSWORD'; // Se 'user_password' não estiver definido, use um valor padrão
+
     cy.get('#user_login').type(email, { log: false });
     cy.get('#user_password').type(password, { log: false });
     cy.get('#new_user > .submit-container > .btn').click();
 })
 
-
 Cypress.Commands.add('logout', () => {
     cy.get('.header-user-dropdown-toggle').click();
     cy.get('.sign-out-link').click();
-
 })
